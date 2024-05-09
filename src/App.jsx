c489a3a383326c700609957af422802951cad1a8
zhramohammadi <zahrakiianfar@gmail.com>
2024-05-09 16:29:07 +0330

    packages added and navbar create

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Container } from 'react-bootstrap'
+import Navbar from './components/navbar'
+
+function App(){
+  return(
+    <Container>
+      <Navbar></Navbar>
+    </Container>
+  )
+}
+
+export default App
